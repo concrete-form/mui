fdaf8acd96239e0aaedf4ea12e7de9cfd2db91f7
Kevin Gilbert <kevin@kegisiteweb.com>
2021-11-25 16:51:40 -0500

    Added control, tests and stories for Checkbox

---
+++ src/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,44 @@
+import { Story } from '@storybook/react'
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder'
+import Favorite from '@mui/icons-material/Favorite'
+import { formContext, FormContextArgs, argTypes } from '../storybook/formContext'
+import Checkbox, { CheckboxProps } from './Checkbox'
+
+export default {
+  component: Checkbox,
+  title: 'Controls Group/Checkbox',
+  decorators: [formContext],
+  argTypes,
+}
+
+const defaultOptions = [
+  'first',
+  { label: 'with label', value: 'second' },
+  { label: <span style={{ fontWeight: 'bold', color: 'deeppink' }}>with style</span>, value: 'third' },
+  { label: 'disabled', value: 'fourth', props: { disabled: true } },
+  'last',
+]
+
+const template: Story<FormContextArgs<CheckboxProps>> = ({ formContext, ...props }) => <Checkbox {...props} />
+
+export const DefaultControl = template.bind({})
+
+DefaultControl.args = {
+  name: 'demo',
+  fieldProps: { required: 'This field is required' },
+  formContext: { defaultValues: { demo: 'second' } },
+  options: defaultOptions,
+}
+
+export const CustomIcon = template.bind({})
+
+CustomIcon.args = {
+  name: 'demo2',
+  orientation: 'horizontal',
+  labelPosition: 'top',
+  icon: <FavoriteBorder />,
+  checkedIcon: <Favorite />,
+  fieldProps: { required: 'This field is required' },
+  formContext: { defaultValues: { demo2: 'second' } },
+  options: defaultOptions,
+}
---
+++ src/Checkbox/Checkbox.tsx
@@ -0,0 +1,97 @@
+import { useMemo, useEffect, useState } from 'react'
+import {
+  CheckboxProps as CoreCheckboxProps,
+  useControlProps,
+  useControlState,
+  useControlActions,
+  parseCheckboxOptions,
+} from '@concrete-form/core'
+import MuiCheckbox, { CheckboxProps as MuiCheckboxProps } from '@mui/material/Checkbox'
+import { FormControlProps } from '@mui/material/FormControl'
+
+import ControlWithErrors from '../util/ControlWithErrors'
+import { getControlledProps } from '../util/controlledProps'
+import { fixMuiLabelPosition } from '../util/labelPosition'
+import ItemsGroup from '../layout/ItemsGroup'
+import ItemLabel from '../layout/ItemLabel'
+
+type PartialMuiCheckboxProps = Omit<MuiCheckboxProps, 'checked'|'defaultChecked'|'id'|'value'|'ref'>
+
+export type CheckboxProps = CoreCheckboxProps<PartialMuiCheckboxProps, React.ReactNode> & {
+  formControlProps?: FormControlProps
+} & PartialMuiCheckboxProps
+
+const Checkbox: React.FC<CheckboxProps> = ({
+  name,
+  options,
+  orientation,
+  labelPosition,
+  formControlProps,
+  ...inputProps
+}) => {
+  const props = useControlProps(name, inputProps, true)
+  const { value: fieldValue } = useControlState(name)
+  const { setFieldValue } = useControlActions(name)
+  const parsedOptions = useMemo(() => parseCheckboxOptions(options), [options])
+  const [value, setValue] = useState(fieldValue ?? [])
+  const styles = useMemo(() => fixMuiLabelPosition(orientation, labelPosition), [orientation, labelPosition])
+
+  useEffect(() => {
+    setValue(fieldValue ?? [])
+  }, [fieldValue])
+
+  const isChecked = (checkboxValue: string) => !!value?.includes(checkboxValue)
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => {
+    const checkboxValue = event.target.value
+    let updatedValues = [...value]
+    if (checked) {
+      updatedValues.push(checkboxValue)
+    } else {
+      updatedValues = updatedValues.filter((existingValue: string) => existingValue !== checkboxValue)
+    }
+    setValue(updatedValues)
+    setFieldValue(updatedValues)
+  }
+
+  // fixme: Mui doesn't seem to handle the input ref properly, forcing us to use controlled input
+  const controlledProps = getControlledProps(props, inputProps, { onChange })
+
+  return (
+    <ControlWithErrors name={name} {...formControlProps}>
+      <ItemsGroup
+        name={name}
+        orientation={orientation}
+        style={styles.group}
+        items={(
+          <>
+            { parsedOptions.map(({ label, value, props: checkboxCustomProps }) => (
+              <ItemLabel
+                key={value}
+                name={name}
+                style={styles.item}
+                control={(
+                  <MuiCheckbox
+                    style={styles.input}
+                    {...controlledProps}
+                    {...checkboxCustomProps}
+                    inputProps={{
+                      ...props.inputProps,
+                      'aria-invalid': props['aria-invalid'],
+                    }}
+                    value={value}
+                    checked={isChecked(value)}
+                  />
+                )}
+                label={label}
+                labelPosition={labelPosition}
+              />
+            )) }
+          </>
+        )}
+      />
+    </ControlWithErrors>
+  )
+}
+
+export default Checkbox
