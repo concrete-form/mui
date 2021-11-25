9d664ccc5c4a5373705121ab81295c8342901f06
Kevin Gilbert <kevin@kegisiteweb.com>
2021-11-25 16:57:40 -0500

    Added controlled props util to avoid duplicating the logic on all controlled inputs

---
+++ src/util/controlledProps.ts
@@ -0,0 +1,26 @@
+import {
+  removeEventHandlers,
+  extractEventHandlers,
+  mergeEventHandlers,
+} from '@concrete-form/core'
+
+type AnyProps = Record<string, any>
+
+/**
+ * 1) This remove event handler and ref from form handler
+ * 2) This preserv custom event handler
+ * 3) this merged controlled event handler with custom event handler
+ */
+export const getControlledProps = (props: AnyProps, inputProps: AnyProps, eventHandler: AnyProps) => {
+  const mergedEvents = Object.fromEntries(
+    Object.entries(eventHandler)
+      .map(([eventName, eventHandler]) => [eventName, mergeEventHandlers(inputProps[eventName], eventHandler)]),
+  )
+
+  return {
+    ...removeEventHandlers(props),
+    ...extractEventHandlers(inputProps),
+    ...mergedEvents,
+    ref: undefined,
+  }
+}
