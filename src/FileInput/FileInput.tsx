3c0ca9bbcdd703507edeeea800cb7be4527a07d0
Kevin Gilbert <kevin@kegisiteweb.com>
2021-11-30 10:37:17 -0500

    Fixed default value of fileinput to avoid console warnings

--- src/FileInput/FileInput.tsx
+++ src/FileInput/FileInput.tsx
@@ -8,6 +8,6 @@ export type FileInputProps = CoreFileInputProps & Omit<InputProps, 'type'>
 
 const FileInput: React.FC<FileInputProps> = ({
   ...inputProps
-}) => <Input {...inputProps} type="file" />
+}) => <Input {...inputProps} type="file" value={undefined} defaultValue="" />
 
 export default FileInput
